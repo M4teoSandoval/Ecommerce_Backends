7cf85fa1a05dccdbe38cfe2a5210521594256ad6
Mateo Sandoval <ksandoval712@unab.edu.co>
2025-09-09 15:45:12 -0500

    Layouts en Laravel

--- resources/views/Products/detail.blade.php
+++ resources/views/Products/detail.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ecommerce</title>
-</head>
-<body>
-
-    <h1> Product detail {{$id}} </h1>
+@extends('layouts.app')
+@section('content')
+    < <h1> Product detail {{$id}} </h1>
     <h1> Con category {{$category}} </h1>
     
-</body>
-</html>
+@endsection
