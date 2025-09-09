bc84711b9196ac27cc7f8cd3903eac791e4cd6e0
Mateo Sandoval <ksandoval712@unab.edu.co>
2025-09-09 13:52:50 -0500

    Rutas Basicas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/products', function () {
+    return 'List Products';
+});
+
+
+
+Route::get('/products/{id}/{category?}', function ($id, $category = null) {
+
+    if ($category != null){
+        return 'Detail Products: ' . $id . ". with category: " . $category;
+        
+    }
+    else{
+
+        return 'Detail Products: ' . $id;
+
+        
+
+    }
+    
+});
